332bda213c9601ab7fcfb51490cb10d4d540c960
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-09-08 18:03:46 -0500

    types in graphql

--- app.js
+++ app.js
@@ -6,16 +6,34 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHTTP({
   schema: buildSchema(`
+
+  type Event{
+    _id: ID!
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
+  input EventInput {
+    title: String!
+    description: String!
+    price: Float!
+    date: String!
+  }
+
   type RootQuery {
-    events: [String!]!
+    events: [Event!]!
   }
 
   type RootMutation{
-    createEvent(name: String):String
+    createEvent(eventInput: EventInput):Event
   }
 
   schema {
@@ -25,11 +43,19 @@ app.use('/graphql', graphqlHTTP({
 `),
   rootValue: {
     events: () => {
-      return ['Cena Romantica', 'Codear toda la noche', 'Practicando ingles']
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        // date: new Date().toISOString()
+        date: args.eventInput.date
+      }
+      events.push(event);
+      return event;  
     }
   },
   graphiql: true
