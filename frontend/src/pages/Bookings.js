1f80f94f29594539321fd5fb20f0aa78e75fc853
Alejandro Aragon <ing.aragon.ipn@gmail.com>
2020-10-27 00:49:31 -0600

    adding delete Booking

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,4 +1,5 @@
 import React, {useContext, useEffect} from 'react'
+import BookingList from '../components/Bookings/BookingList/BookingList';
 import Spiner from '../components/Spiner/Spiner';
 import {AuthContext} from '../context/auth-context';
 import {BookContext} from '../context/book-context';
@@ -49,16 +50,50 @@ function BookingsPage() {
     })
   }, [])
 
+  const deleteBookingHandler = bookingId => {
+    setIsLoading(true);
+    const requestBody = {
+      query:`
+        mutation {
+          cancelBooking(bookingId:"${bookingId}") {
+            _id
+            title
+          }
+        }
+      `
+    };     
+        
+    fetch('http://localhost:8000/graphql',{
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers:{
+        'Content-Type':'application/json',
+        'Authorization' : 'Bearer ' + tokenData.token
+      }
+    }).then(res=>{
+      if(res.status !== 200 && res.status !== 201){
+        throw new Error('Failed');
+      }
+      return res.json();
+    }).then(resData=>{
+      const newBooks = bookings.filter((booking)=>{
+        return booking._id != bookingId;
+      })      
+      setBookings(newBooks);
+      setIsLoading(false);
+    }).catch(err=>{
+      console.log(err);
+      setIsLoading(false);
+    })
+  }
+  
   return (
     <React.Fragment>
-    {isLoading ? <Spiner/> : <ul>
-      { bookings.map(booking=>(
-      <li key={booking._id}>
-        {booking.event.title} - {' '}
-        {new Date(booking.createdAt).toLocaleDateString()}
-      </li>
-      ))}
-    </ul>}    
+      {isLoading ? <Spiner/> :
+      (
+      <BookingList bookings={bookings} onDelete={deleteBookingHandler}/>
+      )
+      }
     </React.Fragment>
   )
 }
